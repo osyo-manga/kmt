0c7aead3c06df0319b27ac1c927791d083e93f43
Komatsu Daisuke <manga.osyo@gmail.com>
2011-04-13 12:46:00 +0900

    _expr 関数を追加。

--- kmt/switch_case.hpp
+++ kmt/switch_case.hpp
@@ -120,11 +120,11 @@ private:
 	value_type value_;
 };
 
-template<typename T, typename Pred = boost::is_same<mpl::_1, mpl::_2> >
+template<typename Pred>
 struct case_type{
-	template<typename U>
-	bool equal(const U&) const{
-		return mpl::apply<Pred, U, T>::type::value;
+	template<typename T>
+	bool equal(const T&) const{
+		return mpl::apply<Pred, T>::type::value;
 	}
 };
 
@@ -143,16 +143,16 @@ struct case_impl{
 	: case_(case__), next_(next){}
 	
 	template<typename new_nextT>
-	case_impl<case_type, new_nextT>
+	case_impl<this_type, new_nextT>
 	operator |=(const new_nextT& next) const{
-		return case_impl<case_type, new_nextT>(case_, next);
+		return case_impl<this_type, new_nextT>(*this, next);
 	}
 	
 	result_type
 	operator ()(){
 		return get_next()();
 	}
-
+	
 	template<typename U>
 	result_type
 	visit(const U& value){
@@ -178,28 +178,32 @@ private:
 
 //----------------------------------------------------------
 // case_expression_impl
-template<typename caseT, typename nextT, typename expressionT>
+template<typename caseT, typename expressionT>
 struct case_expression_impl{
 	typedef caseT       case_type;
 	typedef expressionT expression_type;
-	typedef nextT       next_type;
+	typedef case_expression_impl<case_type, expression_type> this_type;
 	typedef typename expression_type::result_type result_type;
+	typedef typename case_type::next_type next_type;
 	
 	explicit case_expression_impl(
 		const case_type& case__,
-		const next_type& next,
 		expression_type expression
 	)
-	: case_(case__), expression_(expression), next_(next){}
-	
+	: case_(case__), expression_(expression){}
 	template<typename new_nextT>
-	case_expression_impl<case_type, new_nextT, expression_type>
+	case_expression_impl<case_impl<case_type, new_nextT>, expression_type>
 	operator |=(const new_nextT& next) const{
-		return case_expression_impl<case_type, new_nextT, expression_type>
-			(case_, next, expression_);
+		return case_expression_impl<case_impl<case_type, new_nextT>, expression_type>
+			(case_impl<case_type, new_nextT>(case_, next), expression_);
 	}
-	
-	
+/*
+	template<typename new_nextT>
+	case_impl<this_type, new_nextT>
+	operator |=(const new_nextT& next) const{
+		return case_impl<this_type, new_nextT>(*this, next) ;
+	}
+*/
 	result_type
 	operator ()(){
 		return expression_();
@@ -208,49 +212,55 @@ struct case_expression_impl{
 	template<typename U>
 	result_type
 	visit(const U& value){
-		return call_func(value, *this, next_);
+		return call_func(value, *this, case_.get_next());
 	}
 	
 	template<typename T>
 	bool
 	equal(const T& t) const{
-		return case_.equal(t);;
+		return case_.equal(t);
 	}
+	
+	next_type& get_next(){
+		return case_.get_next();
+	}
+	next_type const& get_next() const{
+		return case_.get_next();
+	}
+
 private:
 	case_type case_;
 	expression_type expression_;
-	next_type next_;
 };
 
 
-
 template<typename T, typename next_type, typename F>
-case_expression_impl<case_impl<T, next_type>, next_type, eval_impl<F&> >
+case_expression_impl<case_impl<T, next_type>, eval_impl<F&> >
 operator |(case_impl<T, next_type> const& case_, F& f){
-	return case_expression_impl<case_impl<T, next_type>, next_type, eval_impl<F&> >
-		(case_, case_.get_next(), eval_impl<F&>(f));
+	return case_expression_impl<case_impl<T, next_type>, eval_impl<F&> >
+		(case_, eval_impl<F&>(f));
 }
 
 template<typename T, typename next_type, typename F>
-case_expression_impl<case_impl<T, next_type>, next_type, eval_impl<F const&> >
+case_expression_impl<case_impl<T, next_type>, eval_impl<F const&> >
 operator |(case_impl<T, next_type> const& case_, F const& f){
-	return case_expression_impl<case_impl<T, next_type>, next_type, eval_impl<F const&> >
-		(case_, case_.get_next(), eval_impl<F const&>(f));
+	return case_expression_impl<case_impl<T, next_type>, eval_impl<F const&> >
+		(case_, eval_impl<F const&>(f));
 }
 
 
 template<typename T, typename next_type, typename U>
-case_expression_impl<case_impl<T, next_type>, next_type, value_impl<U&> >
+case_expression_impl<case_impl<T, next_type>, value_impl<U&> >
 operator &(case_impl<T, next_type> const& case_, U& value){
-	return case_expression_impl<case_impl<T, next_type>, next_type, value_impl<U> >
-		(case_, case_.get_next(), value_impl<U>(value));
+	return case_expression_impl<case_impl<T, next_type>, value_impl<U> >
+		(case_, value_impl<U>(value));
 }
 
 template<typename T, typename next_type, typename U>
-case_expression_impl<case_impl<T, next_type>, next_type, value_impl<U const&> >
+case_expression_impl<case_impl<T, next_type>, value_impl<U const&> >
 operator &(case_impl<T, next_type> const& case_, U const& value){
-	return case_expression_impl<case_impl<T, next_type>, next_type, value_impl<U const&> >
-		(case_, case_.get_next(), value_impl<U const&>(value));
+	return case_expression_impl<case_impl<T, next_type>, value_impl<U const&> >
+		(case_, value_impl<U const&>(value));
 }
 
 
@@ -279,12 +289,6 @@ template<typename T>
 bool operator ==(const true_&, const T&){
 	return true;
 }
-std::ostream&
-operator <<(std::ostream& os, const true_&){
-	os << true;
-	return os;
-}
-
 
 template<typename T>
 struct functionable_holder{
@@ -307,6 +311,7 @@ private:
 static const detail::case_impl<detail::true_> default_(_);
 
 }  // namespace detail
+
 
 using detail::_;
 using detail::default_;
@@ -336,11 +341,19 @@ case_(const T& t){
 	return detail::case_impl<detail::case_value<T> >(detail::case_value<T>(t));
 }
 
-template<typename T>
-detail::case_impl<detail::case_type<T> >
-case_(){
-	return detail::case_impl<detail::case_type<T> >(detail::case_type<T>());
+template<typename Pred>
+detail::case_impl<detail::case_type<Pred> >
+case_type(){
+	return detail::case_impl<detail::case_type<Pred> >(detail::case_type<Pred>());
 }
+
+template<typename T>
+detail::case_impl<detail::case_type<boost::is_same<T, boost::mpl::_1> > >
+case_(){
+	return case_type<boost::is_same<T, boost::mpl::_1> >();
+}
+
+
 
 
 // 引数が2個以上の場合は、boost::tuple で保持
@@ -355,6 +368,70 @@ template<typename T1, typename T2>
 detail::case_impl<detail::case_value<boost::tuple<T1, T2> > >
 case_(const T1& t1, const T2& t2){
 	return case_( boost::make_tuple(t1, t2) );
+}
+
+
+//----------------------------------------------------------
+// expr
+template<typename T, typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<T> >,
+	detail::eval_impl<F&>
+>
+case_expr(const T& t, F& f){
+	return detail::case_expression_impl<
+		detail::case_impl<detail::case_value<T> >,
+		detail::eval_impl<F&>
+	>(detail::case_impl<detail::case_value<T> >(t), detail::eval_impl<F&>(f));
+}
+
+
+template<typename T, typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<T> >,
+	detail::eval_impl<F const&>
+>
+case_expr(const T& t, F const& f){
+	return detail::case_expression_impl<
+		detail::case_impl<detail::case_value<T> >,
+		detail::eval_impl<F const&>
+	>(detail::case_impl<detail::case_value<T> >(t), detail::eval_impl<F const&>(f));
+}
+
+template<typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<detail::true_> >,
+	detail::eval_impl<F&>
+>
+default_expr(F& f){
+	return case_expr(detail::_, f);
+}
+
+template<typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<detail::true_> >,
+	detail::eval_impl<F const&>
+>
+default_expr(F const& f){
+	return case_expr(detail::_, f);
+}
+
+template<typename T1, typename T2, typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<boost::tuple<T1, T2> > >,
+	detail::eval_impl<F&>
+>
+case_expr(const T1& t1, const T2& t2, F& f){
+	return case_expr( boost::make_tuple(t1, t2), f);
+}
+
+template<typename T1, typename T2, typename F>
+detail::case_expression_impl<
+	detail::case_impl<detail::case_value<boost::tuple<T1, T2> > >,
+	detail::eval_impl<F const&>
+>
+case_expr(const T1& t1, const T2& t2, F const& f){
+	return case_expr( boost::make_tuple(t1, t2), f);
 }
 
 }  // namespace switch_case
--- libs/switch_case/example/switch_case.cpp
+++ libs/switch_case/example/switch_case.cpp
@@ -9,13 +9,41 @@
 #include <boost/mpl/print.hpp>
 #include <boost/variant.hpp>
 
+#include <boost/mpl/print.hpp>
+#include <boost/variant.hpp>
+#include <boost/mpl/push_back.hpp>
+#include <boost/mpl/unique.hpp>
+#include <boost/mpl/placeholders.hpp>
+#include <boost/mpl/remove.hpp>
+
+
 namespace sc = kmt::switch_case;
+
+template<typename T, typename Seq = boost::mpl::vector<> >
+struct result{
+	typedef typename boost::mpl::push_back<
+		typename result<typename T::next_type, Seq>::type0,
+		typename T::result_type
+	>::type type0;
+	typedef typename boost::mpl::unique<
+		type0, 
+		boost::is_same<boost::mpl::_1, boost::mpl::_2>
+	>::type type1;
+	typedef typename boost::mpl::remove<type1, void>::type type;
+
+};
+
+template<typename Seq>
+struct result<sc::detail::empty_case, Seq>{
+	typedef Seq type0;
+};
+
 
 template<typename T>
 void
 hoge(T t){
 	using boost::mpl::print;
-//	typedef typename print<T>::type Ttype;
+///	typedef typename print<T>::type Ttype;
 }
 
 void
@@ -27,7 +55,9 @@ test(){
 //	switch_(0)|=case_(0)|=case_(1)|=case_(2);
 	int n = switch_(0)
 		|=case_(0)
-		|=case_(1)|var(1);
+		|=case_(1)|var(1)
+		|=case_(2)
+		|=case_(3)&0;
 	std::cout << n << std::endl;
 }
 
@@ -51,14 +81,29 @@ void
 fizz_buzz(int n){
 	using sc::_;
 	
-	std::string result = sc::switch_(n%3, n%5)
+	std::string result
+	= sc::switch_(n%3, n%5)
 		|=sc::case_(0,0)|sc::var(std::string("fizz_buzz"))
-		|=sc::case_(0,_)&(std::string("fizz"))
+		|=sc::case_(0,_)|sc::var(std::string("fizz"))
 		|=sc::case_(_,0)&(std::string("buzz"))
 		|=sc::default_&(boost::lexical_cast<std::string>(n));
 	std::cout << result << std::endl;
 }
 
+
+#include <boost/lambda/lambda.hpp>
+
+void
+fizz_buzz_disp(int n){
+	using boost::lambda::constant;
+	using sc::_;
+
+	sc::switch_(n%3, n%5)
+		|=sc::case_expr(0,0, std::cout << constant(std::string("fizz_buzz")) << "\n")
+		|=sc::case_expr(0,_, std::cout << constant(std::string("fizz")) << "\n")
+		|=sc::case_expr(_,0, std::cout << constant(std::string("buzz")) << "\n")
+		|=sc::default_expr(std::cout << constant(boost::lexical_cast<std::string>(n)) << "\n");
+}
 
 int
 main(){
@@ -76,11 +121,12 @@ main(){
 		|=sc::case_(std::string("yes"))
 		|=sc::case_(std::string("y"))
 		|=sc::case_(std::string("Y"))|sc::var(std::string("ok"))
-		|=sc::default_|sc::var(std::string("error"));
+		|=sc::default_&std::string("error");
 	std::cout << result << std::endl;
 	
 	for(int i = 1 ; i < 20 ; ++i){
 		fizz_buzz(i);
+		fizz_buzz_disp(i);
 	}
 	
 	return 0;
