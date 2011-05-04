d048af4177746a40e7064cd23f582eb93c5ded09
Komatsu Daisuke <manga.osyo@gmail.com>
2011-05-04 23:50:01 +0900

    case_pred を追加

---
+++ libs/switch_case/example/case_pred.cpp
@@ -0,0 +1,45 @@
+//
+// kmt-library
+//
+// Copyright (c) 2011 
+// osyo-manga : http://d.hatena.ne.jp/osyo-manga/
+// 
+// Readme:
+// https://github.com/osyo-manga/kmt/blob/master/README
+//
+// License:
+// Boost Software License - Version 1.0
+// <http://www.boost.org/LICENSE_1_0.txt>
+//
+#include <kmt/switch_case/include/switch_case.hpp>
+
+#include <boost/lambda/lambda.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/range/algorithm/for_each.hpp>
+
+#include <string>
+#include <iostream>
+
+struct disp{
+	disp(std::string str) : str_(str){};
+	
+	void operator ()() const{
+		std::cout << str_ << std::endl;
+	}
+private:
+	std::string str_;
+};
+
+int
+main(){
+	
+	namespace sc = kmt::switch_case;
+	using boost::lambda::_1;
+	
+	boost::for_each(boost::irange(0, 10),
+		  sc::case_pred(_;1 % 2 == 0)|disp("even")
+		|=sc::case_    (sc::_)       |disp("odd")
+	);
+	
+	return 0;
+}
