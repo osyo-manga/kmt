82e4e6a17a22546100a112ebbb0aab7896a5a781
Komatsu Daisuke <manga.osyo@gmail.com>
2011-05-18 17:20:09 +0900

    c_array を追加。

---
+++ kmt/mpl/c_array.hpp
@@ -0,0 +1,109 @@
+//
+// kmt-library
+//
+// Copyright (c) 2011 
+// osyo-manga : http://d.hatena.ne.jp/osyo-manga/
+// 
+// Readme:
+// https://github.com/osyo-manga/kmt/blob/master/README
+//
+// License:
+// Boost Software License - Version 1.0
+// <http://www.boost.org/LICENSE_1_0.txt>
+//
+#ifndef KMT_MPL_C_ARRAY_H
+#define KMT_MPL_C_ARRAY_H
+
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/at.hpp>
+#include <boost/array.hpp>
+
+#include <kmt/utility/value_init.hpp>
+#include <boost/preprocessor/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/enum.hpp>
+
+
+#ifndef KMT_MPL_C_ARRAY_LIMIT_SIZE
+#    define KMT_MPL_C_ARRAY_LIMIT_SIZE 100
+#endif
+
+
+#if defined BOOST_NO_INITIALIZER_LISTS
+	// initializer_list が実装されていない場合は、Boost.Assign を使用して、初期化を行う
+	#include <boost/assign.hpp>
+	#define PP_C_ARRAY_AC_C(z, n, text) \
+		(boost::mpl::at_c<Seq, n>::type::value)
+	
+	#define PP_C_ARRYA_AC_C_INITIALIZER(n)	\
+		boost::assign::list_of BOOST_PP_REPEAT_FROM_TO(0, n, PP_C_ARRAY_AC_C, _)
+#else
+	// initializer_list が実装されている場合は、initializer_list を使用する
+	#define PP_C_ARRAY_AC_C(z, n, text) \
+		boost::mpl::at_c<Seq, n>::type::value
+	
+	#define PP_C_ARRYA_AC_C_INITIALIZER(n)	\
+		{ BOOST_PP_ENUM(n, PP_C_ARRAY_AC_C, _)  }
+#endif
+
+
+namespace kmt{ namespace mpl{
+
+
+namespace detail{
+
+template<typename T, typename Size>
+struct array_type
+	: boost::mpl::identity<boost::array<T, Size::value> >{};
+
+template<
+	typename Seq, long Size,
+	typename ArrayType
+>
+struct c_array_impl;
+
+#define PP_C_ARRAY(z, n, text)                              \
+template<                                                   \
+	typename Seq,                                           \
+	typename ArrayType                                      \
+>                                                           \
+struct c_array_impl<Seq, n, ArrayType>{                     \
+	typedef c_array_impl<Seq, n, ArrayType> type;           \
+	                                                        \
+	typedef typename boost::mpl::apply<                     \
+		ArrayType,                                          \
+		typename Seq::value_type, boost::mpl::size<Seq>     \
+	>::type value_type;                                     \
+	                                                        \
+	static value_type const value;                          \
+	                                                        \
+	operator value_type const&() const{                     \
+		return value;                                       \
+	}                                                       \
+};                                                          \
+                                                            \
+template<typename Seq, typename ArrayType>                  \
+typename c_array_impl<Seq, n, ArrayType>::value_type const  \
+c_array_impl<Seq, n, ArrayType>::value = PP_C_ARRYA_AC_C_INITIALIZER(n);
+
+
+BOOST_PP_REPEAT_FROM_TO(1, KMT_MPL_C_ARRAY_LIMIT_SIZE, PP_C_ARRAY, _)
+
+#undef PP_C_ARRAY
+#undef PP_C_ARRAY_AC_C
+#undef PP_C_ARRYA_AC_C_INITIALIZER
+
+}  // namespace detail
+
+template<
+	typename Seq,
+	typename ArrayType = detail::array_type<boost::mpl::_1, boost::mpl::_2>
+>
+struct c_array
+	: detail::c_array_impl<Seq, boost::mpl::size<Seq>::value, ArrayType>{};
+
+
+} } // namespace kmt{ namespace mpl{
+
+#endif /* KMT_MPL_C_ARRAY_H */
---
+++ libs/mpl/test/c_array.cpp
@@ -0,0 +1,60 @@
+//
+// kmt-library
+//
+// Copyright (c) 2011 
+// syo-manga : http://d.hatena.ne.jp/osyo-manga/
+// 
+// Readme:
+// https://github.com/osyo-manga/kmt/blob/master/README
+//
+// License:
+// Boost Software License - Version 1.0
+// <http://www.boost.org/LICENSE_1_0.txt>
+//
+#include <boost/test/minimal.hpp>
+
+#include <kmt/mpl/c_array.hpp>
+
+#include <boost/mpl/vector_c.hpp>
+#include <boost/mpl/range_c.hpp>
+#include <boost/mpl/assert.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+#include <boost/range/algorithm/equal.hpp>
+
+
+#include <vector>
+
+
+namespace mpl = boost::mpl;
+namespace km  = kmt::mpl;
+
+typedef mpl::vector_c<int, 3, 5, 2, 1, 4> seq;
+
+BOOST_MPL_ASSERT(( boost::is_same<
+	km::c_array<seq>::value_type,
+	boost::array<int, 5>
+> ));
+
+BOOST_MPL_ASSERT(( boost::is_same<
+	km::c_array<seq, std::vector<mpl::_1> >::value_type,
+	std::vector<int>
+> ));
+
+
+int
+test_main(int argc, char* argv[]){
+	
+	BOOST_CHECK(( boost::equal(
+		km::c_array<mpl::vector_c<int, 0, 1, 2, 3, 4> >::value,
+		km::c_array<mpl::range_c<int, 0, 5> >::value
+	) ));
+	\
+	boost::array<int, 5> array = {{ 3, 5, 2, 1, 4 }};
+	BOOST_CHECK(( boost::equal(
+		array, km::c_array<seq>::value
+	) ));
+	
+	return 0;
+}
+
