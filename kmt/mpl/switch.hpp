1263f234381afd4a68e06bc6d90dcef04014da08
Komatsu Daisuke <manga.osyo@gmail.com>
2011-05-25 07:34:25 +0900

    mpl::switch に case と default を追加。

---
+++ kmt/mpl/switch.hpp
@@ -0,0 +1,43 @@
+//
+// kmt-library
+//
+// Copyright (c) 2011 
+// osyo-manga : http://d.hatena.ne.jp/osyo-manga/
+// 
+// Readme:
+// https://github.com/osyo-manga/kmt/blob/master/README
+//
+// License:
+// Boost Software License - Version 1.0
+// <http://www.boost.org/LICENSE_1_0.txt>
+//
+#ifndef KMT_MPL_SWITCH_H
+#define KMT_MPL_SWITCH_H
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/mpl/switch.hpp>
+#include <boost/mpl/always.hpp>
+#include <boost/mpl/lambda.hpp>
+
+namespace kmt{ namespace mpl{
+
+template<typename F, typename S, typename T = void>
+struct case_;
+
+template<typename F, typename S>
+struct case_<F, S,
+	typename boost::enable_if<boost::mpl::is_lambda_expression<S> >::type>
+	: boost::mpl::pair<F, S> {};
+
+template<typename F, typename S>
+struct case_<F, S,
+	typename boost::disable_if<boost::mpl::is_lambda_expression<S> >::type>
+	: boost::mpl::pair<F, boost::mpl::always<S> > {};
+
+template<typename S>
+struct default_ : boost::mpl::pair<boost::mpl::always<boost::mpl::true_>, S>{};
+
+
+} } // namespace kmt{ namespace mpl{
+
+#endif /* KMT_MPL_SWITCH_H */
